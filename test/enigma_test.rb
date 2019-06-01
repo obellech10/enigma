8341f68d69ff86beb4988da1b7ac0e943189c91d
Oscar Bellechasse <obellech10@gmail.com>
2019-06-01 15:20:14 -0600

    Add test for instance of class

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,18 @@
 require 'simplecov'
 SimpleCov.start
+
+require 'minitest'
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/enigma'
+
+class EnigmaTest < MiniTest::Test
+
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_that_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
