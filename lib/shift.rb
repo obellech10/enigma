43a2b1620383637ee6d48ed295d9cabbb106a6ab
Oscar Bellechasse <obellech10@gmail.com>
2019-06-02 15:30:18 -0600

    Add comments on future modifications for assign_shift method

--- lib/shift.rb
+++ lib/shift.rb
@@ -5,9 +5,11 @@ class Shift
     @offset = offset
   end
 
+# Will need to change to output just an array or change how keys are assigned
   def assign_shift
     @key.assign_keys.merge(@offset.assign_offset) do |key_id, keys, shift|
       keys + shift
-    end  
+    end
   end
+
 end
