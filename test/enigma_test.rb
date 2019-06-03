cdeddc7f8aebd41308565f6195b560a2b2bd0e3e
Oscar Bellechasse <obellech10@gmail.com>
2019-06-03 08:49:09 -0600

    Edit test case for test_that_it_can_encrypt

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -17,9 +17,6 @@ class EnigmaTest < MiniTest::Test
 
   def setup
     @enigma = Enigma.new
-    @message = Convert.new("Hello World")
-    @keys = Keys.new("02715")
-    @date = Offset.new("040895")
   end
 
   def test_that_it_exists
@@ -30,6 +27,6 @@ class EnigmaTest < MiniTest::Test
     expected = {decryption: "keder ohulw",
                 key: "02715",
                 date: "040895"}
-    assert_equal expected, @enigma.encrypt(@message, @keys, @date)
+    assert_equal expected, @enigma.encrypt("Hello World", "02715", "040895")
   end
 end
