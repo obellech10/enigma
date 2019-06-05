539244bc3e0f2a63f2dc71cc97acd2599d2977df
Oscar Bellechasse <obellech10@gmail.com>
2019-06-04 18:11:57 -0600

    Add test that it can square date

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -13,32 +13,36 @@ require 'pry'
 
 class OffsetTest < MiniTest::Test
 
-  def setup
-    @offset = Offset.new("040895")
-    @offset_2 = Offset.new
-  end
-
   def test_that_it_exists
-    assert_instance_of Offset, @offset
+    offset = Offset.new("040895")
+    assert_instance_of Offset, offset
   end
 
   def test_attributes
-    assert_equal "040895", @offset.date
+    offset = Offset.new("040895")
+    assert_equal "040895", offset.date
   end
 
   def test_that_it_can_assign_offsets_to_keys
+    offset = Offset.new("040895")
     expected = {
       a: 1,
       b: 0,
       c: 2,
       d: 5
     }
-    assert_equal expected, @offset.assign_offset
+    assert_equal expected, offset.assign_offset
+  end
+
+  def test_that_it_can_square_date
+    offset = Offset.new("281276")
+    assert_equal "8176", offset.squared_date
   end
 
   def test_that_it_can_generate_date
+    offset = Offset.new("281276")
     Offset.any_instance.stubs(:generate_date).returns("020619")
-    assert_equal "020619", @offset.generate_date
+    assert_equal "020619", offset.generate_date
   end
 
 end
